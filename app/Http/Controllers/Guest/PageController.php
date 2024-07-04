45e78edf4bfb636de9666a47cba75a9645475c50
michele laptop <micheledong2803@gmail.com>
2024-07-04 17:17:02 +0200

    m3 - index()

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index(){
+        $data = config("data");
+        return view('home',$data);
+    }
 }
